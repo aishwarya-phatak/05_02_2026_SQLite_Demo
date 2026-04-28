040963b4d7ba2fe3f083406589763de4256fe9c1
aishwarya <ashp17star@gmail.com>
2026-04-28 12:12:33 +0530

    SQLITE Demo

--- app/src/main/java/com/example/a05_02_2026_sqlite_demo/DBUtil.kt
+++ app/src/main/java/com/example/a05_02_2026_sqlite_demo/DBUtil.kt
@@ -3,6 +3,7 @@ package com.example.a05_02_2026_sqlite_demo
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.util.Log
 
 class DBUtil(context: Context) {
     var dbHelper: SQLiteDatabase = DBHelper(
@@ -18,32 +19,74 @@ class DBUtil(context: Context) {
         empCity: String
     ) {
         val contentValues = ContentValues()
-        contentValues.put("empId",empId)
-        contentValues.put("empName",empName)
-        contentValues.put("empCity",empCity)
+        contentValues.put("empId", empId)
+        contentValues.put("empName", empName)
+        contentValues.put("empCity", empCity)
 
-        dbHelper.insert("Employee",
+        dbHelper.insert(
+            "Employee",
             null,
-            contentValues)
-        for(i in 1..5){
+            contentValues
+        )
+        for (i in 1..5) {
             val contentValues = ContentValues()
-            contentValues.put("empId","emp $i")
-            contentValues.put("empName","emp name $i")
-            contentValues.put("empCity","emp city $i")
+            contentValues.put("empId", "emp $i")
+            contentValues.put("empName", "emp name $i")
+            contentValues.put("empCity", "emp city $i")
+            dbHelper.insert("Employee", null, contentValues)
         }
     }
 
-    fun deleteEmployee(empId : Int){
-
+    fun deleteEmployee(empId: Int) {
+        val numberOfRowsDeleted = dbHelper.delete(
+            "Employee",
+            "empId = ?",
+            arrayOf(empId.toString())
+        )
+        Log.e("number of rows deleted is :", "$numberOfRowsDeleted")
     }
 
-    fun updateEmployeeRecord(empId : Int,
-                             empName : String,
-                             empCity : String){
+    fun updateEmployeeRecord(
+        empId: Int,
+        empName: String,
+        empCity: String
+    ) {
+        val contentValues = ContentValues()
+        contentValues.put("empId", empId)
+        contentValues.put("empName", empName)
+        contentValues.put("empCity", empCity)
 
+        val updatedRows = dbHelper.update(
+            "Employee",
+            contentValues,
+            "empId = ?",
+            arrayOf(empId.toString())
+        )
+        Log.e("updated rows : ","$updatedRows")
     }
 
-    fun getAllEmployees(){
+    fun getAllEmployees() {
+        val employees = ArrayList<Employee>()
+        val cursor = dbHelper.query(
+            "Employee",
+            null,
+            null,
+            null,
+            null,
+            null,
+            "empId desc"
+        )
 
+        while (cursor.moveToNext()) {
+            val empId = cursor.getInt(0)
+            val empName = cursor.getString(1)
+            val empCity = cursor.getString(2)
+
+            val employeeObject = Employee(empId, empName, empCity)
+            employees.add(employeeObject)
+        }
+
+        val length = employees.size
+        Log.e("tag", "${employees.toString()}")
     }
 }
--- app/src/main/java/com/example/a05_02_2026_sqlite_demo/MainActivity.kt
+++ app/src/main/java/com/example/a05_02_2026_sqlite_demo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.a05_02_2026_sqlite_demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +13,23 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         val dbUtil = DBUtil(this)
-        dbUtil.insertEmployeeRecords(12454,
-            "Suved",
-            "Pune")
+//        dbUtil.insertEmployeeRecords(90231,
+//            "Ashish",
+//            "Pune")
+
+        Log.e("tag","------------------------")
+        dbUtil.getAllEmployees()
+
+        Log.e("tag","------------------------")
+        dbUtil.deleteEmployee(90231)
+
+        Log.e("tag","------------------------")
+        dbUtil.getAllEmployees()
+
+        Log.e("tag","------------------------")
+        dbUtil.updateEmployeeRecord(12454,"Rahul","Banglore")
+
+        Log.e("tag","------------------------")
+        dbUtil.getAllEmployees()
     }
 }
